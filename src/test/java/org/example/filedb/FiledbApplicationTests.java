3147c6d88282be27346bfda18c54b629519cfc68
ErikSvensson76 <eosvensson@gmail.com>
2024-09-18 14:17:28 +0200

    Uploaded to github

---
+++ src/test/java/org/example/filedb/FiledbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.filedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiledbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
