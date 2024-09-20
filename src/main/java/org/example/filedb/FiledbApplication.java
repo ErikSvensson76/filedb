90940511a7c17bb4364f9d4366c4519a5514f174
ErikSvensson76 <eosvensson@gmail.com>
2024-09-20 08:49:57 +0200

    Updated main file

--- src/main/java/org/example/filedb/FiledbApplication.java
+++ src/main/java/org/example/filedb/FiledbApplication.java
@@ -1,9 +1,16 @@
 package org.example.filedb;
 
+import org.example.filedb.config.BaseUrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties(
+    value = BaseUrlConfig.class
+)
 public class FiledbApplication {
 
   public static void main(String[] args) {
