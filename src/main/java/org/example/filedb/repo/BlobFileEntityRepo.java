21161792a77ed683854945da5fabfcd467c99be6
ErikSvensson76 <eosvensson@gmail.com>
2024-09-20 14:45:46 +0200

    Added BlobFileEntityRepo.findByFilename method

--- src/main/java/org/example/filedb/repo/BlobFileEntityRepo.java
+++ src/main/java/org/example/filedb/repo/BlobFileEntityRepo.java
@@ -3,5 +3,8 @@ package org.example.filedb.repo;
 import org.example.filedb.model.BlobFileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BlobFileEntityRepo extends JpaRepository<BlobFileEntity, String> {
+  Optional<BlobFileEntity> findByFilename(String fileName);
 }
